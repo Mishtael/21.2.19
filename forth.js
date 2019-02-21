95598547ece2161fef44ad3ee56f330afe49daa6
Mishtael <37247168+Mishtael@users.noreply.github.com>
2019-02-21 20:51:56 +0200

    Create forth.js

---
+++ forth.js
@@ -0,0 +1,13 @@
+var x = parseInt(prompt("please enter number",0))
+if (x<10)
+{
+ 
+  alert("1")
+}
+  else
+      if (x<100)
+      {
+        alert("2")
+        }
+        else
+          alert("3")

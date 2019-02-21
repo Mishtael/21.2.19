de919206d07295736c04c3a15deca2df1455b569
Mishtael <37247168+Mishtael@users.noreply.github.com>
2019-02-21 20:27:09 +0200

    Create first.js

---
+++ first.js
@@ -0,0 +1,10 @@
+var x = parseInt(prompt("please enter first number", 0));
+var y = parseInt(prompt("please enter second number", 0));
+if (x>y)
+  {
+    alert(x);
+  }
+else
+  {
+    alert(y);
+  }
